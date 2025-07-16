c9dc29e0d3f6d94aedd7a7105f2dbe325415eb4a
Andy Osei <andrewowusu21@yahoo.com>
2025-07-16 08:43:42 +0000

    logout issues

--- hooks/useLogout.ts
+++ hooks/useLogout.ts
@@ -16,7 +16,7 @@ export const useLogout = () => {
       sessionStorage.clear();
       router.replace("/login");
       window.location.reload();
-    }, 50);
+    }, 150);
   };
 
   return handleLogout;
